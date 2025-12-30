f3e7b7383bce3d9bfda598acaf788d6e0ebecbc4
DedsecKnight <mikenguyen7268@gmail.com>
2025-12-29 20:33:45 -0600

    use profile instead

--- scripts/build-debug.sh
+++ scripts/build-debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-conan install . --build=missing -s build_type=Debug
+conan install . --build=missing -s build_type=Debug -pr:h=profiles/profile_linux_x86 -pr:b=profiles/profile_linux_x86
 cmake --preset conan-debug
 cmake --build --preset conan-debug
 
