30f7d3b8e5c36fa17acfe636c3a6752b3cdb3299
DedsecKnight <mikenguyen7268@gmail.com>
2026-01-04 11:43:28 -0600

    refactored processor name storage mechanism

--- apps/simple-app/main.cpp
+++ apps/simple-app/main.cpp
@@ -31,10 +31,9 @@ class pong_processor : public multithreaded::events::handlers<pong_event> {
 
 int main(int argc, char** argv) {
   multithreaded::engine e{};
-  using namespace std::literals;
 
-  auto p1 = e.create_processor<ping_processor>("p1"sv);
-  auto p2 = e.create_processor<pong_processor>("p2"sv);
+  auto p1 = e.create_processor<ping_processor>("p1");
+  auto p2 = e.create_processor<pong_processor>("p2");
 
   p2.subscribe_to<pong_event>(p1);
   p1.subscribe_to<ping_event>(p2);
