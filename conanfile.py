9c28ef798356dfcae1f23999a568bcd37a3b2ea5
DedsecKnight <mikenguyen7268@gmail.com>
2026-01-03 18:09:51 -0600

    reduced dependencies

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class VideoPlayerRecipe(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("ffmpeg/7.1.3")
+        self.requires("ffmpeg/8.0.1")
         self.requires("cli11/2.6.0")
         self.requires("spdlog/1.16.0")
         self.requires("opencv/4.12.0")
@@ -19,6 +19,17 @@ class VideoPlayerRecipe(ConanFile):
     def configure(self):
         self.options["ffmpeg"].with_pulse = False
         self.options["sdl"].shared = True
+        self.options["opencv"].ml = False
+        self.options["opencv"].dnn = False
+        self.options["opencv"].stitching = False
+        self.options["opencv"].dnn = False
+        self.options["opencv"].calib3d = False
+        self.options["opencv"].highgui = False
+        self.options["opencv"].videoio = False
+        self.options["opencv"].objdetect = False
+        self.options["opencv"].features2d = False
+        self.options["opencv"].with_ffmpeg = False
+        self.options["opencv"].with_quirc = False
 
     def generate(self):
         tc = CMakeToolchain(self, generator="Ninja")
