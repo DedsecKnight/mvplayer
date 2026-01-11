da50cf5d8ee140bc6eb36c2c5fcbe849a55d196a
DedsecKnight <mikenguyen7268@gmail.com>
2026-01-10 20:20:58 -0600

    made write port classes clang-tidy compliant

--- include/multithreaded/connector/write/event_port.hpp
+++ include/multithreaded/connector/write/event_port.hpp
@@ -2,16 +2,16 @@
 
 namespace multithreaded::write {
 template <typename event_t>
-class event_port {
+class event_port {  // NOLINT
  public:
   virtual ~event_port() = default;
-  virtual bool send_event(const event_t& e) noexcept = 0;
+  virtual bool send_event(const event_t& event) noexcept = 0;
 };
 
 template <typename port_t, typename event_t>
 class event_port_impl : public event_port<event_t> {
-  bool send_event(const event_t& e) noexcept override {
-    return static_cast<port_t*>(this)->push(e);
+  bool send_event(const event_t& event) noexcept override {
+    return static_cast<port_t*>(this)->push(event);
   }
 };
 }  // namespace multithreaded::write
