6593f55129d9d3a156acd3e9ad74e2cc95457899
DedsecKnight <mikenguyen7268@gmail.com>
2026-01-10 20:14:51 -0600

    made read port classes clang-tidy compliant

--- include/multithreaded/connector/read/event_port.hpp
+++ include/multithreaded/connector/read/event_port.hpp
@@ -2,16 +2,17 @@
 
 namespace multithreaded::read {
 template <typename event_t>
-class event_port {
+class event_port {  // NOLINT
  public:
   virtual ~event_port() = default;
   virtual bool get_event(event_t& elem) noexcept = 0;
 };
 
+// TODO: figure out whether we should define move semantics and copy semantics
 template <typename port_t, typename event_t>
-class event_port_impl : public event_port<event_t> {
+class event_port_impl : public event_port<event_t> {  // NOLINT
  public:
-  bool get_event(event_t& event) noexcept override {
+  [[nodiscard]] bool get_event(event_t& event) noexcept override {
     return static_cast<port_t*>(this)->pop(event);
   }
 
