b902619549e89f7de79b1809701c53a74d9eef8d
DedsecKnight <mikenguyen7268@gmail.com>
2026-02-28 09:18:00 -0600

    combined event handling and frame rendering into 1 thread

--- include/mvplayer/utils/constants.hpp
+++ include/mvplayer/utils/constants.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <chrono>
 #include <cstddef>
-
-using namespace std::chrono_literals;
 
 namespace mvplayer::constants {
 static constexpr size_t CACHE_LINE_SIZE = 64;
-static constexpr std::chrono::milliseconds POLL_INTERVAL = 14ms;
 }  // namespace mvplayer::constants
